16c819578f0065007d58b31c4dffdf7cb9ce0817
om1nus <tooxplayz@gmail.com>
2020-12-13 21:36:43 +0000

    first commit

---
+++ home.js
@@ -0,0 +1,7 @@
+
+$(".searchbtn").click(function(){
+    $(this).toggleClass("bg-green");
+    $(".fas").toggleClass("color-white");
+    $(".input").focus().toggleClass("active-width").val('');
+  });
+
